18c20b39f924f06fc37062aeb8fddf90ee9c5b44
Tyron Boyd <tyronboyd@Tyrons-MBP.gateway>
2017-03-29 19:57:55 +1100

    changed urls again

--- src/main/typescript/app/util/constants.ts
+++ src/main/typescript/app/util/constants.ts
@@ -1,7 +1,7 @@
 export class Constants {
 
-  public static get WHITTAM_REST_URL(): string { return "http://localhost:8080/whittam-ropes/" };
-  public static get WHITTAM_WEBSOCKET_URL(): string { return "ws://localhost:8080/whittam-ropes/order-added" };
+  public static get WHITTAM_REST_URL(): string { return "http://192.168.0.59:8080/whittam-ropes/" };
+  public static get WHITTAM_WEBSOCKET_URL(): string { return "ws://192.168.0.59:8080/whittam-ropes/order-added" };
 
 
 }
